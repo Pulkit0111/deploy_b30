3b6db00929a45126d827ad4f689b7c182392307e
Pulkit0111 <ptyagi1551@gmail.com>
2023-10-26 12:21:53 +0530

    ready for deployment

---
+++ index.js
@@ -0,0 +1,22 @@
+const express=require("express")
+const {connection}=require("./db")
+require("dotenv").config()
+const {userRouter}=require("./routes/user.routes")
+const {noteRouter}=require("./routes/note.rouets")
+
+const app=express()
+
+app.use(express.json())
+
+app.use("/users",userRouter)
+app.use("/notes",noteRouter)
+
+app.listen(process.env.port,async()=>{
+    try{
+        await connection
+        console.log("Connected to the DB")
+        console.log(`Server is running at port ${process.env.port}`)
+    } catch(err){
+        console.log(err)
+    }
+})
